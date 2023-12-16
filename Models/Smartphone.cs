aa63a2012edaed255bba7d356e2fae56aea6e528
DanielMarcos1 <daniel04pessoal@gmail.com>
2023-12-16 08:59:01 -0300

    Entrega do projeto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,21 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // DONE: Implementar as propriedades faltantes de acordo com o diagrama
+        private string modelo;
+        private string iMEI;
+        private int memoria;
+        protected string Modelo { get => modelo; set => modelo = value; }
+        protected string IMEI { get => iMEI; set => iMEI = value; }
+        protected int Memoria { get => memoria; set => memoria = value; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // DONE: Passar os parâmetros do construtor para as propriedades
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("+55 11 99973-4531", "iPhone 14 PRO MAX", "0849751127584612309", 128);
+iphone.Ligar();
+// Joãozinho não atendeu, mas retornou a ligação
+iphone.ReceberLigacao();
+// Joãozinho pediu para instalarmos o app para jogar juntos
+iphone.InstalarAplicativo("8 Ball Pool - Sinuca Online");
+
+Nokia nokia = new Nokia("+55 11 98856-7594", "Nokia G60 5G", "5128764756948655239456", 128);
+nokia.Ligar();
+// Roberto não atendeu, mas retornou a ligação
+nokia.ReceberLigacao();
+// Roberto nos pediu para instalar o instagram
+nokia.InstalarAplicativo("Instagram");
